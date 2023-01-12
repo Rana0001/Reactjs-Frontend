6922c38025b379c518449434487c58ab13762cde
Rana0001 <magarprashant001@gmail.com>
2023-01-12 08:28:56 +0545

    Changes

--- src/App.js
+++ src/App.js
@@ -1,36 +1,82 @@
 import './App.css';
 import { useState } from "react";
-import Feedback from './components/feedback';
-function App() {
-  // const{name,age}=props
+// import feedback from './components/feedback';
+function App(props) {
 
-  const [counter, setCounter]= useState(0)
-  // setTimeout(
-  //   ()=> setCounter(counter +1),
-  //   1000)
+  const [notes,setNotes] = useState(props.notes)
+  const [newNote,setNewNote] = useState('')
+  const [showAll, setShowAll]= useState(true)
 
-  const handleClick=()=> setCounter(counter +1)
-  // function handleClick(){
-  //   return setCounter(counter +1)
-  // }
-  const handleReset=()=> setCounter(0)
+const notesToShow = showAll 
+? notes
+: notes.filter(n=> n.important === true)  
+
+  const handleChange = (event)=>{
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+
+  const handleAdd = (event)=>{
+    event.preventDefault()
+    console.log(event.target)
+
+    const note = {
+      id:notes.length+1,
+      content:newNote,
+      date: new Date().toISOString(),
+      important: Math.random()<0.5,
+    }
+    if(newNote !== "") setNotes(notes.concat(note))
+    
+    setNewNote('')
+  }
+  // // const{name,age}=props
+  // const [counter, setCounter]= useState(0)
+  // // setTimeout(
+  // //   ()=> setCounter(counter +1),
+  // //   1000)
+
+  // const handleClick=()=> setCounter(counter +1)
+  // // function handleClick(){
+  // //   return setCounter(counter +1)
+  // // }
+  // const handleReset=()=> setCounter(0)
   
-  console.log(`rendering ${counter}`)
+  // console.log(`rendering ${counter}`)
 
-  return (
-    <>
-  {/* <h1>Hello {name}, You are {age} years old.</h1> */} 
-    <h2>{counter}</h2>
-    <button onClick={handleClick}>Plus</button>
-    <button onClick={handleReset}>Reset</button>
+  // return (
+  //   <>
+  // {/* <h1>Hello {name}, You are {age} years old.</h1> */} 
+  //   <h2>{counter}</h2>
+  //   <button onClick={handleClick}>Plus</button>
+  //   <button onClick={handleReset}>Reset</button>
     
 
-    <Feedback>
+  //   <Feedback>
       
-    </Feedback>
+  //   </Feedback>
 
+  //   </>
+  // );
+  return(
+    <>
+    <h2>Notes </h2>
+    <button onClick={()=> setShowAll(!showAll)}>Show All </button>
+
+    <ul>
+    {notesToShow.map(note => 
+    <li key={note.id}>{note.content} <p>{note.date}</p></li>
+    
+    )}
+    </ul>
+
+    <form>
+      <input value={newNote} onChange={handleChange}/>
+      <button onClick={handleAdd}>Add</button>
+    </form>
     </>
-  );
+  )
 }
 
 export default App;
